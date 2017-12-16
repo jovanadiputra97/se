1c9c43a82e0281911690f83760cbef22bfe37fc4
Jovanadiputra97 <Jovanadiputra97@gmail.com>
2017-12-17 00:28:00 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// --------------------
+// Backpack\Demo routes
+// --------------------
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => ['admin'],
+    'namespace'  => 'Admin',
+], function () {
+    // CRUD resources and other admin routes
+    CRUD::resource('monster', 'MonsterCrudController');
+});
+
+Route::get('api/article', 'Api\ArticleController@index');
+Route::get('api/article/{id}', 'Api\ArticleController@show');
